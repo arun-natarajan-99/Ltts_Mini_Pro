fbd07a4d7555e7b9af412ce53f6d54ebde5dc34b
arun-natarajan-99 <80808104+arun-natarajan-99@users.noreply.github.com>
2021-04-16 15:59:36 +0530

    Update election.c

--- 3_Implementation/test/election.c
+++ 3_Implementation/test/election.c
@@ -1,20 +1,11 @@
-#include "unity"
-
-
-#define CANDIDATE_COUNT
-
-#define CANDIDATE1 "David Hull"
-#define CANDIDATE2 "Kristin Canella"
-#define CANDIDATE3 "Jim Brar"
-#define CANDIDATE4 "Donald Truimph"
+#include "unity.h"
+#include "acutest.h"
+#include "functions.h"
 
 
 
 void castVote();
-
-
 void votesCount();
-
 void getLeadingCandidate();
 
 void setUp(){}
@@ -30,27 +21,48 @@ int main()
   RUN_TEST(votesCount);
   RUN_TEST(getLeadingCandidate);
   
+TEST_ASSERT_EQUAL_INT( 1, void castVote(void) );
+ TEST_ASSERT_EQUAL_INT( 2, void votesCount(void) ); 
+ TEST_ASSERT_EQUAL_INT( 3, void getLeadingCandidate(void) );
 
   return UNITY_END();
 }
 
+
 void castVote(void) {
-  TEST_ASSERT_EQUAL(1, if(1);
-  
-  /* Dummy fail*/
+  int TEST_ASSERT_EQUAL(votesCount1++, 1);
+  int TEST_ASSERT_EQUAL(votesCount2++, 2);
+  int TEST_ASSERT_EQUAL(votesCount3++, 3);
+  int TEST_ASSERT_EQUAL(votesCount4++, 4);
+  int TEST_ASSERT_EQUAL();
+
   TEST_ASSERT_EQUAL(15000, add(7500, 7500));
+
 }
+  TEST_LIST = {
+{"int add(int, int);", casteVote},
+{0}
+}; 
 
 void votesCount(void) {
   TEST_ASSERT_EQUAL(3, add(0, 3));
-  
-  /* Dummy fail*/
+  int TEST_ASSERT_EQUAL();
+ 
   TEST_ASSERT_EQUAL(100, add(1000, 900));
+ 
 }
+ TEST_LIST = {
+{"int add(int, int);", votesCount},
+{0}
+}; 
 
 void getLeadingCandidate(void) {
-  TEST_ASSERT_EQUAL(0, max(1, 0));
+  TEST_ASSERT_EQUAL(1, max(1, 0));
   
-  /* Dummy fail*/
+  
   TEST_ASSERT_EQUAL(10, max(2, 5));
 }
+TEST_LIST = {
+{"int max(int, int);", getLeadingCandidate},
+{0}
+}; 
